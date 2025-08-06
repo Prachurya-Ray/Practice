f6cb24cba5cf0fa5bbd0fb64e18e7ac29ea2d1c4
Prachurya Ray <prachuryaray@gmail.com>
2025-08-07 00:00:57 +0530

    basic routing done

--- mini-blog-app/src/components/Header.jsx
+++ mini-blog-app/src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div className='flex justify-between  py-4 px-8 bg-blue-500 text-white'>
-        <h1 className='font-bold text-xl'>Mini Blog</h1>
-        <ul className="menus flex gap-10 font-semibold">
-            <li>Home</li>
-            <li>Create Blog</li>
-        </ul>
+    <div className="flex justify-between  py-4 px-8 bg-blue-500 text-white">
+      <h1 className="font-bold text-xl">Mini Blog</h1>
+      <ul className="menus flex gap-10 font-semibold">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/blogCreation">Create Blog</Link>
+        </li>
+        <li></li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Header
--- mini-blog-app/src/main.jsx
+++ mini-blog-app/src/main.jsx
@@ -1,16 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter } from 'react-router-dom'
-import './index.css'
-import Body from './components/Body.jsx'
-import Header from './components/Header.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Body from "./components/Body.jsx";
+import Header from "./components/Header.jsx";
+import CreateBlog from "./components/CreateBlog.jsx";
 
-const AppLayout =()=>{
-  return <>
-   <Header />
-    <Body /></>
-}
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet/>
+    </>
+  );
+};
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path:'/blogCreation',
+        element: <CreateBlog/>
+      }
+    ],
+  },
+]);
 
-
-createRoot(document.getElementById('root')).render(<AppLayout/>)
+createRoot(document.getElementById("root")).render(<RouterProvider router={appRouter}/>);
