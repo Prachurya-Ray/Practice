42a3736fc7505e9ba5d58929d55b2f63dc899c9c
Prachurya Ray <prachuryaray@gmail.com>
2025-08-06 23:49:04 +0530

    basic page created

--- mini-blog-app/src/main.jsx
+++ mini-blog-app/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import Body from './components/Body.jsx'
 import Header from './components/Header.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Header />
-    <Body />
-  </StrictMode>,
-)
+const AppLayout =()=>{
+  return <>
+   <Header />
+    <Body /></>
+}
+
+
+
+createRoot(document.getElementById('root')).render(<AppLayout/>)
